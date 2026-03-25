7b04b15b398e773d232ce15768e9d9e0b3044263
StivensCh <gschavez@ucundinamarca.edu.co>
2026-03-25 17:41:22 -0500

    cambios Dto

--- src/main/java/com/mascotas/Mascotas/dto/MascotaDto.java
+++ src/main/java/com/mascotas/Mascotas/dto/MascotaDto.java
@@ -1,10 +1,14 @@
 package com.mascotas.Mascotas.dto;
 
 import com.mascotas.Mascotas.model.Mascota;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MascotaDto {
     private String nombre;
     private  String codigo;
